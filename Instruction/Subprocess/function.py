c4ad16d167bd6f4a227c409251d1351ebf591963
Conor <conor@johngregg.org>
2022-02-25 11:53:27 -0600

    Added function params

--- Instruction/Subprocess/function.py
+++ Instruction/Subprocess/function.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from Instruction.instruction import Instruction
 from Instruction.Subprocess.subprocess import Subprocess
 
@@ -5,8 +7,17 @@ from Instruction.Subprocess.subprocess import Subprocess
 class Function(Subprocess):
     # TODO: function params
     instructions: list[Instruction]
+    required_variables: dict[str, Any]
     name: str
 
     def __init__(self, name: str):
         self.instructions = []
+        self.required_variables = {}
         self.name = name
+
+    def add_required_input(self, name):
+        self.required_variables[name] = None
+
+    def execute(self, context) -> None:
+        if None in self.required_variables.values():
+            raise ValueError("Missing required input")
